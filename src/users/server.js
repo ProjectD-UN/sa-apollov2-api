e08c68be12a28acea2fa847982e187326c12f952
Diego Rodriguez <diefrodriguezcha@unal.edu.co>
2018-12-08 23:27:01 -0500

    Changed url env variable in users

--- src/users/server.js
+++ src/users/server.js
@@ -1,3 +1,3 @@
-export const user_url = process.env.URL
+export const user_url = process.env.USERS_URL
 export const user_port = process.env.USERS_PORT
 export const user_entryPoint = process.env.USERS_ENTRY
